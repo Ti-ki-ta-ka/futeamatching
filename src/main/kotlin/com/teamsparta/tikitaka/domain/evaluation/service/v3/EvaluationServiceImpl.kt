d6100ec14a3dff373465ada6ac4e55a107406bb1
HJH1111 <162549235+HJH1111@users.noreply.github.com>
2024-08-12 15:46:04 +0900

    :sparkles: 매치 성사시 평가를 하라는 이메일 송신 기능 추가 (#142)
    Co-authored-by: Jiho <https://github.com/HJH1111>

--- src/main/kotlin/com/teamsparta/tikitaka/domain/evaluation/service/v3/EvaluationServiceImpl.kt
+++ src/main/kotlin/com/teamsparta/tikitaka/domain/evaluation/service/v3/EvaluationServiceImpl.kt
@@ -6,9 +6,13 @@ import com.teamsparta.tikitaka.domain.evaluation.dto.EvaluationRequest
 import com.teamsparta.tikitaka.domain.evaluation.dto.EvaluationResponse
 import com.teamsparta.tikitaka.domain.evaluation.model.Evaluation
 import com.teamsparta.tikitaka.domain.evaluation.repository.EvaluationRepository
+import com.teamsparta.tikitaka.domain.evaluation.repository.SuccessMatchRepository
 import com.teamsparta.tikitaka.domain.match.model.SuccessMatch
+import com.teamsparta.tikitaka.domain.team.repository.TeamRepository
+import com.teamsparta.tikitaka.domain.users.dto.EmailDto
 import com.teamsparta.tikitaka.domain.users.repository.UsersRepository
 import com.teamsparta.tikitaka.infra.security.UserPrincipal
+import jakarta.mail.MessagingException
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -19,6 +23,7 @@ class EvaluationServiceImpl(
     private val evaluationRepository: EvaluationRepository,
     private val usersRepository: UsersRepository,
     private val successMatchRepository: SuccessMatchRepository,
+    private val evaluationEmailService: EvaluationEmailService
 ) : EvaluationService {
 
     @Transactional
@@ -45,18 +50,46 @@ class EvaluationServiceImpl(
     }
 
     @Transactional
-    override fun createEvaluationsForMatch(match: SuccessMatch) {
+    override fun calculateAndUpdateScores() {
+        val now = LocalDateTime.now()
+        val startDate = now.minusDays(90)
+        val endDate = now.withHour(0).withMinute(0).withSecond(0).withNano(0)
+
+        val teams = teamRepository.findAll()
+
+        teams.forEach { team ->
+            val evaluations = evaluationRepository.findEvaluationsForTeamFromLast90Days(team.id!!, startDate, endDate)
+            val totalMannerScore = evaluations.sumOf { it.mannerScore }
+            val totalSkillScore = evaluations.sumOf { it.skillScore }
+            val totalAttendanceScore = evaluations.sumOf { it.attendanceScore }
+            val evaluationCount = evaluations.size
+
+            team.mannerScore = if (evaluationCount == 0) 0 else totalMannerScore
+            team.tierScore = if (evaluationCount == 0) 0 else totalSkillScore
+            team.attendanceScore = if (evaluationCount == 0) 0 else totalAttendanceScore
+
+            teamRepository.save(team)
+        }
+    }
+    
+    private val verificationEmail = mutableMapOf<String, String>()
+
+    @Transactional
+    override fun createEvaluationsForMatch(match: SuccessMatch): EmailDto {
+
         val hostTeamEvaluation = Evaluation(
             evaluatorTeamId = match.hostTeamId,
             evaluateeTeamId = match.guestTeamId,
-            evaluatorId = match.guestId,
+            evaluatorId = match.hostId,
             createdAt = LocalDateTime.now(),
+            email = match.hostEmail
         )
         val guestTeamEvaluation = Evaluation(
             evaluatorTeamId = match.guestTeamId,
             evaluateeTeamId = match.hostTeamId,
-            evaluatorId = match.hostId,
+            evaluatorId = match.guestId,
             createdAt = LocalDateTime.now(),
+            email = match.guestEmail
         )
 
         evaluationRepository.save(hostTeamEvaluation)
@@ -65,7 +98,17 @@ class EvaluationServiceImpl(
         match.evaluationCreatedTrue()
         successMatchRepository.save(match)
 
+        val message = evaluationEmailService.sendMessage()
+        verificationEmail[hostTeamEvaluation.email] = message
+        verificationEmail[guestTeamEvaluation.email] = message
 
+        try {
+            evaluationEmailService.sendEmail(hostTeamEvaluation.email, message)
+            evaluationEmailService.sendEmail(guestTeamEvaluation.email, message)
+        } catch (e: MessagingException) {
+            throw IllegalArgumentException("메일 발송 중 오류가 발생했습니다.")
+        }
+        return EmailDto(message)
     }
 
     @Transactional
