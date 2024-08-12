94636669c4a3ca2f707e6dced1269e608025a847
임성우 <seongwoorang@naver.com>
2024-08-12 15:49:15 +0900

    ✨ #138 - 날씨 API 연동 (#145)
    * :sparkles: #138 - 기상청 단기 예보 API 연동
    
    * :sparkles: #138 - SecurityConfig Endpoint 추가
    
    * :sparkles: #138 - data type xml -> json으로 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")// 액추에이터
     implementation("io.micrometer:micrometer-registry-prometheus") // 프로메테우스 마이크로미터 구현체 추가
 
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
     kapt("com.querydsl:querydsl-apt:$queryDslVersion:jakarta")
     kapt("jakarta.annotation:jakarta.annotation-api")
     kapt("jakarta.persistence:jakarta.persistence-api")
@@ -77,7 +78,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-aop")
 
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
-
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
@@ -90,6 +90,7 @@ dependencies {
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
     runtimeOnly("org.postgresql:postgresql")
+    runtimeOnly("com.h2database:h2")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
 }
