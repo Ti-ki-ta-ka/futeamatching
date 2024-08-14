b6305049685198eaa5ad165a8245c3ecad052227
HJH1111 <162549235+HJH1111@users.noreply.github.com>
2024-08-14 14:38:29 +0900

    :adhesive_bandage: EmailService에 setContext함수 불필요 삭제 (#182)
    Co-authored-by: Jiho <https://github.com/HJH1111>

--- src/main/kotlin/com/teamsparta/tikitaka/domain/users/service/v3/EmailService.kt
+++ src/main/kotlin/com/teamsparta/tikitaka/domain/users/service/v3/EmailService.kt
@@ -1,6 +1,5 @@
 package com.teamsparta.tikitaka.domain.users.service.v3
 
-
 import jakarta.mail.internet.MimeMessage
 
 interface EmailService {
@@ -8,5 +7,4 @@ interface EmailService {
     fun sendEmail(email: String)
     fun verificationEmail(email: String, number: String): Boolean
     fun makeMemberId(email: String): String
-    fun setContext(code: String): String
 }
