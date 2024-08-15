2612f129c3688a92388fb80664a8530149dfb224
JinkwonHong <161419351+JinkwonHong@users.noreply.github.com>
2024-08-15 15:38:01 +0900

    ✨ #186 - Team 내부 TierScore를 기준으로, Ranking, RegionRanking 반영 (#188)
    * :sparkles: #186 - Ranking System 도입 및 Step 별 Class 분리
    
    * :package: #186 - Batch Package 구조 변경
    
    * :recycle: #186 - RegionRanking Inner Class ➡️ Nested Class (중첩 Class)로 변경

--- src/main/kotlin/com/teamsparta/tikitaka/infra/batch/config/TeamEvaluationStepConfig.kt
+++ src/main/kotlin/com/teamsparta/tikitaka/infra/batch/config/TeamEvaluationStepConfig.kt
@@ -1,4 +1,4 @@
-package com.teamsparta.tikitaka.infra.batch
+package com.teamsparta.tikitaka.infra.batch.config
 
 import com.teamsparta.tikitaka.domain.common.exception.ModelNotFoundException
 import com.teamsparta.tikitaka.domain.evaluation.model.Evaluation
@@ -6,79 +6,33 @@ import com.teamsparta.tikitaka.domain.evaluation.model.QEvaluation.evaluation
 import com.teamsparta.tikitaka.domain.evaluation.repository.EvaluationRepository
 import com.teamsparta.tikitaka.domain.team.model.Team
 import com.teamsparta.tikitaka.domain.team.repository.TeamRepository
+import com.teamsparta.tikitaka.infra.batch.listener.StepPerformanceListener
+import com.teamsparta.tikitaka.infra.batch.reader.QuerydslZeroOffsetItemReader
 import jakarta.persistence.EntityManagerFactory
-import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.StepScope
-import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.item.ItemProcessor
-import org.springframework.batch.item.ItemReader
 import org.springframework.batch.item.ItemWriter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
 
 @Configuration
-class BatchConfig(
+class TeamEvaluationStepConfig(
     private val evaluationRepository: EvaluationRepository,
     private val teamRepository: TeamRepository,
     private val jobRepository: JobRepository,
     private val transactionManager: PlatformTransactionManager,
-    private val entityManagerFactory: EntityManagerFactory
+    private val entityManagerFactory: EntityManagerFactory,
 ) {
-
-    @Bean
-    fun teamEvaluationJob(clearTeamScoresStep: Step, teamEvaluationStep: Step): Job {
-        return JobBuilder("teamEvaluationJob", jobRepository).start(clearTeamScoresStep).next(teamEvaluationStep)
-            .build()
-    }
-
-    @Bean
-    fun clearTeamScoresStep(): Step {
-        return StepBuilder("clearTeamScoresStep", jobRepository).chunk<Team, Team>(100, transactionManager)
-            .reader(clearTeamScoresItemReader()).processor(clearTeamScoresItemProcessor())
-            .writer(clearTeamScoresItemWriter()).build()
-    }
-
-    @StepScope
-    @Bean
-    fun clearTeamScoresItemReader(): ItemReader<Team> {
-        val teams = teamRepository.findAll().iterator()
-        return ItemReader {
-            if (teams.hasNext()) {
-                teams.next()
-            } else {
-                null
-            }
-        }
-    }
-
-    @StepScope
-    @Bean
-    fun clearTeamScoresItemProcessor(): ItemProcessor<Team, Team> {
-        return ItemProcessor { team ->
-            team.mannerScore = 0
-            team.tierScore = 0
-            team.attendanceScore = 0
-            team
-        }
-    }
-
-    @StepScope
-    @Bean
-    fun clearTeamScoresItemWriter(): ItemWriter<Team> {
-        return ItemWriter { teams ->
-            teamRepository.saveAll(teams)
-        }
-    }
 
     @Bean
     fun teamEvaluationStep(
         performanceListener: StepPerformanceListener
     ): Step {
-        return StepBuilder("teamEvaluationStep", jobRepository).chunk<Evaluation, Team>(100, transactionManager)
+        return StepBuilder("teamEvaluationStep", jobRepository).chunk<Evaluation, Team>(200, transactionManager)
             .reader(evaluationItemReader(evaluationRepository)).processor(evaluationItemProcessor(teamRepository))
             .writer(teamItemWriter()).listener(performanceListener).build()
     }
@@ -94,7 +48,7 @@ class BatchConfig(
                 query
             },
             idExtractor = { it.id as Long }).apply {
-            setPageSize(100)
+            setPageSize(200)
         }
     }
 
@@ -120,10 +74,5 @@ class BatchConfig(
         return ItemWriter { teams ->
             teamRepository.saveAll(teams)
         }
-    }
-
-    @Bean
-    fun stepPerformanceListener(): StepPerformanceListener {
-        return StepPerformanceListener()
     }
 }
