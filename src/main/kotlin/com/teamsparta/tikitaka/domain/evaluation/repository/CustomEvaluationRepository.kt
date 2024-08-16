ae0dd6d7cadc8fc213eec7de40f4c14ffe3262f1
JinkwonHong <161419351+JinkwonHong@users.noreply.github.com>
2024-08-16 12:06:37 +0900

    :recycle: #189 - Spring Batch Server 분리 (#193)

--- src/main/kotlin/com/teamsparta/tikitaka/domain/evaluation/repository/CustomEvaluationRepository.kt
+++ src/main/kotlin/com/teamsparta/tikitaka/domain/evaluation/repository/CustomEvaluationRepository.kt
@@ -1,11 +1,7 @@
 package com.teamsparta.tikitaka.domain.evaluation.repository
 
-import com.querydsl.jpa.impl.JPAQuery
-import com.querydsl.jpa.impl.JPAQueryFactory
-import com.teamsparta.tikitaka.domain.evaluation.model.Evaluation
 import java.time.LocalDateTime
 
 interface CustomEvaluationRepository {
     fun softDeleteOldEvaluations(threshold: LocalDateTime, now: LocalDateTime)
-    fun findEvaluationsWithPagination(queryFactory: JPAQueryFactory): JPAQuery<Evaluation>
 }
